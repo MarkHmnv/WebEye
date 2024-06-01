68325e2e8cd36e196b767b9d8753bad2dfcb7196
Mark <skrytyjinkognito54@gmail.com>
2024-06-01 18:02:04 +0300

    Added the ability to dynamically create and delete celery beat schedulers using the redbeat library. Added a function for monitoring sites. The function saves a screenshot of a site in cache and then compares it with new incoming screenshots using Mean Squared Error

--- api/monitoring/crud.py
+++ api/monitoring/crud.py
@@ -1,16 +1,23 @@
+from datetime import timedelta
+
+from redbeat import RedBeatSchedulerEntry
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.monitoring.persistence import WebsiteMonitor
 from api.monitoring.schemas import WebsiteMonitorCreate
+from api.core.celery_config import celery_app
+from api.core.constants import WEBSITE_KEY
+from api.core.redis_config import rd
 from api.user.persistence import User
 
 
-async def add(monitoring: WebsiteMonitorCreate, user: User, session: AsyncSession):
+async def add(monitoring: WebsiteMonitorCreate, user: User, session: AsyncSession) -> WebsiteMonitor:
     monitor = WebsiteMonitor(**monitoring.dict(), user_id=user.id)
     session.add(monitor)
     await session.flush()
     await session.commit()
+    return monitor
 
 
 async def find_all_for_user(user: User, session: AsyncSession) -> list[WebsiteMonitor]:
@@ -35,3 +42,26 @@ async def delete(website_monitor: WebsiteMonitor, session):
 async def get_by_id(monitoring_id: int, session: AsyncSession) -> WebsiteMonitor | None:
     query = select(WebsiteMonitor).where(WebsiteMonitor.id == monitoring_id)
     return (await session.execute(query)).scalar()
+
+
+async def find_all_by_user_id(user_id: int, session: AsyncSession) -> list[WebsiteMonitor]:
+    query = select(WebsiteMonitor).where(WebsiteMonitor.user_id == user_id)
+    return list((await session.execute(query)).scalars().all())
+
+
+def add_monitoring_to_scheduler(website_monitor: WebsiteMonitor):
+    RedBeatSchedulerEntry(
+        name=f'monitor_{website_monitor.id}_{website_monitor.user_id}',
+        task='api.monitoring.tasks.monitor_website',
+        args=[website_monitor.url, website_monitor.id, website_monitor.user_id],
+        schedule=timedelta(seconds=website_monitor.interval_minutes),
+        app=celery_app
+    ).save()
+
+
+def delete_monitoring_from_scheduler(website_monitor: WebsiteMonitor):
+    RedBeatSchedulerEntry.from_key(
+        key=f'redbeat:monitor_{website_monitor.id}_{website_monitor.user_id}',
+        app=celery_app
+    ).delete()
+    rd.hdel(WEBSITE_KEY, f'{website_monitor.id}_{website_monitor.user_id}')
