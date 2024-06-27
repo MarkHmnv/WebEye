e6d257bd91d8c40ef4714e9e052cc3b6282bd3c6
Mark <42696773+MarkHmnv@users.noreply.github.com>
2024-06-27 14:24:09 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -20,14 +20,9 @@ routers = (
 )
 include_routers(app, routers)
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
